d9c6a87a5359f36c010bfc8ea8b6077dfccab389
ZuzRad <zradosz00@gmail.com>
2024-09-28 17:38:34 +0200

    app.module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { FormComponent } from './form/form.component';
 import { DetailsComponent } from './details/details.component';
 
 // Import routing module
-//import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    FormComponent,
+    DetailsComponent
+  ],
   imports: [
     BrowserModule,
-    //AppRoutingModule, // Routing module import
+    AppRoutingModule, // Routing module import
     BrowserAnimationsModule, // Required for Angular Material
   ],
   bootstrap: [AppComponent],
