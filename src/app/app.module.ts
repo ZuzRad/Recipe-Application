49364e73ef213e519ec477dd6bb02ce33b71168d
ZuzRad <zradosz00@gmail.com>
2024-09-29 17:19:30 +0200

    Style update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'; 
+
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { provideHttpClient } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
@@ -30,6 +33,7 @@ import { AppRoutingModule } from './app.routing.module';
     BrowserAnimationsModule, 
     MatCardModule,
     MatFormFieldModule,
+    MatToolbarModule,
     MatInputModule,
     MatButtonModule,
   ],
