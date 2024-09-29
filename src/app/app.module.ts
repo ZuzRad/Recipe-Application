e4f62af5fa53c66cc9693ea37788b6bb265d73fb
ZuzRad <zradosz00@gmail.com>
2024-09-29 13:40:38 +0200

    BootstrapModule fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ListComponent } from './list/list.component';
 import { FormComponent } from './form/form.component';
 import { DetailsComponent } from './details/details.component';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
